8e15c96bddc04ad0d083af6a63604d4a36b28846
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-04-29 00:03:16 -0300

    Validação de marcação no mesmo dia e horário

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,20 +1,38 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
 
 const appointmentsRouter = Router();
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
+  const parsedDate = startOfHour(parseISO(date));
+  const appointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (appointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
 
-  response.json(appointment);
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
