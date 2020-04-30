dec37ade33773836da4b724fcc9bb4779c473f88
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-04-29 22:58:01 -0300

    Separando responsabilidade de estrutura dos dados para o model

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,12 +1,6 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
-
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
 const appointments: Appointment[] = [];
@@ -24,11 +18,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
