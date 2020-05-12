009e81376dedd2ddcd8da1810c582d1b034dce54
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-05-12 10:01:25 -0300

    Exporta e reutiliza caminho da imagem

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -2,9 +2,12 @@ import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
+const destinationPath = path.resolve(__dirname, '..', '..', 'tmp');
+
 export default {
+  directory: destinationPath,
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    destination: destinationPath,
     filename(request, file, callback) {
       const hash = crypto.randomBytes(10).toString('HEX');
       const filename = `${hash}-${file.originalname}`;
