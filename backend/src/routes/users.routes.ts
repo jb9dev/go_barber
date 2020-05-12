1de161571bba9c144c81f863e1c14cf8a999dd50
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-05-12 10:01:06 -0300

    Rota para adicionar avatar ao usuário

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenicated from '../middlewares/ensureAuthenicated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -17,5 +21,14 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenicated,
+  upload.single('avatar'),
+  (request, response) => {
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
