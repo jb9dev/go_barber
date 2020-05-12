5ff408b030fd4871e49791938aeea3fdc562e283
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-05-12 08:04:51 -0300

    Implementa middleware que  verifica autenticação

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -5,7 +5,11 @@ import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
+import ensureAuthenicated from '../middlewares/ensureAuthenicated';
+
 const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenicated);
 
 appointmentsRouter.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
@@ -16,12 +20,12 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider_id, date } = request.body;
+    const { date } = request.body;
     const parsedDate = parseISO(date);
 
     const createAppointment = new CreateAppointmentService();
     const appointment = await createAppointment.execute({
-      provider_id,
+      provider_id: request.user.id,
       date: parsedDate,
     });
 
