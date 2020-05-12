d78d8440b5881afb612fad7005685e464f1785ad
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-05-12 13:10:39 -0300

    Criando tratativa de erros de forma global

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,7 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
+import AppError from './errors/AppError';
 import routes from './routes';
 import 'reflect-metadata';
 
@@ -9,6 +12,25 @@ const server = express();
 server.use(express.json());
 server.use(routes);
 
+server.use(
+  (err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    // eslint-disable-next-line
+    console.log(err);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
+
 server.listen('3333', () => {
-  console.log('🚀️ Server running on port 3333!');
+  console.log('🚀️ Server running on port 3333!'); // eslint-disable-line
 });
