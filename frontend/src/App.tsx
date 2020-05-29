4712e572902d4346aadd469d14c30185cbf9e820
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-05-29 10:39:28 -0300

    Configurações iniciais para preparação do projeto

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <React.Fragment>
+    <h1>Go Barber</h1>
+    <GlobalStyle />
+  </React.Fragment>
+)
+
+export default App;
