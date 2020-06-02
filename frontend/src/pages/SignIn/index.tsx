3c852e47d3dfda3cb789e46d61b44c3e83da6289
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-02 08:28:28 -0300

    Implementa o Form do unform/web

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form } from '@unform/web';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import Input from '../../components/Input';
@@ -9,43 +10,49 @@ import logoImg from '../../assets/logo.svg';
 
 import { Container, Content, BackgroundImg } from './styles';
 
-const SingIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img
-        src={logoImg}
-        alt="Logo Awesome Go Barber Haircuts and shaves"
-        title="Logo Go Barber"
-      />
-      <form>
-        <h1>Faça o seu logon</h1>
-        <Input
-          name="email"
-          type="email"
-          placeholder="E-mail"
-          icon={FiMail}
+const SingIn: React.FC = () => {
+  const handleSubmit = (data: object): void => {
+    console.log(data)
+  };
+
+  return (
+    <Container>
+      <Content>
+        <img
+          src={logoImg}
+          alt="Logo Awesome Go Barber Haircuts and shaves"
+          title="Logo Go Barber"
         />
-        <Input
-          name="password"
-          type="password"
-          placeholder="Senha"
-          icon={FiLock}
-        />
-        <Button type="submit">Entrar</Button>
-        <Link
-          to="/forgot-password"
-          className="forgot-password"
-        >
-          Esqueci minha senha
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça o seu logon</h1>
+          <Input
+            name="email"
+            type="email"
+            placeholder="E-mail"
+            icon={FiMail}
+          />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            icon={FiLock}
+          />
+          <Button type="submit">Entrar</Button>
+          <Link
+            to="/forgot-password"
+            className="forgot-password"
+          >
+            Esqueci minha senha
+          </Link>
+        </Form>
+        <Link to="/register" className="register">
+          <FiLogIn />
+          Criar conta
         </Link>
-      </form>
-      <Link to="/register" className="register">
-        <FiLogIn />
-        Criar conta
-      </Link>
-    </Content>
-    <BackgroundImg />
-  </Container>
-);
+      </Content>
+      <BackgroundImg />
+    </Container>
+  );
+};
 
 export default SingIn;
--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form } from '@unform/web';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
 
 import Input from '../../components/Input';
@@ -9,44 +10,50 @@ import logoImg from '../../assets/logo.svg';
 
 import { Container, Content, BackgroundImg } from './styles';
 
-const SingUp: React.FC = () => (
-  <Container>
-    <BackgroundImg />
-    <Content>
-      <img
-        src={logoImg}
-        alt="Logo Awesome Go Barber Haircuts and shaves"
-        title="Logo Go Barber"
-      />
-      <form>
-        <h1>Faça o seu cadastro</h1>
-        <Input
-          name="name"
-          type="text"
-          placeholder="Nome"
-          icon={FiUser}
-        />
-        <Input
-          name="email"
-          type="email"
-          placeholder="E-mail"
-          icon={FiMail}
-        />
-        <Input
-          name="password"
-          type="password"
-          placeholder="Senha"
-          icon={FiLock}
-        />
-        <Button type="submit">Entrar</Button>
-      </form>
-      <Link to="/" className="back-to-logon">
-        <FiArrowLeft />
-        Voltar para logon
-      </Link>
-    </Content>
+const SingUp: React.FC = () => {
+  const handleSubmit = (data: object):void => {
+    console.log('data: ', data);
+  };
 
-  </Container>
-);
+  return (
+    <Container>
+      <BackgroundImg />
+      <Content>
+        <img
+          src={logoImg}
+          alt="Logo Awesome Go Barber Haircuts and shaves"
+          title="Logo Go Barber"
+        />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça o seu cadastro</h1>
+          <Input
+            name="name"
+            type="text"
+            placeholder="Nome"
+            icon={FiUser}
+          />
+          <Input
+            name="email"
+            type="email"
+            placeholder="E-mail"
+            icon={FiMail}
+          />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            icon={FiLock}
+          />
+          <Button type="submit">Entrar</Button>
+        </Form>
+        <Link to="/" className="back-to-logon">
+          <FiArrowLeft />
+          Voltar para logon
+        </Link>
+      </Content>
+
+    </Container>
+  );
+};
 
 export default SingUp;
