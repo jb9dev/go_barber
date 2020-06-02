c7ac98dce35ff027ba91d76ca0eb9f6c1515e08b
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-01 23:48:14 -0300

    Isola o button como um componente separado

---
+++ frontend/src/components/Button/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import { colors } from '../../styles/variables';
+
+const { primary, secondary } = colors;
+
+export const Container = styled.button`
+  width: 100%;
+  height: 47px;
+  margin-top: 20px;
+  padding: 0 16px;
+  color: ${secondary};
+  font-weight: 500;
+  background-color: ${primary};
+  border: 0;
+  border-radius: 6px;
+  transition: background-color 200ms ease-in;
+
+  &:hover {
+    background-color: ${shade(0.2, primary)};
+  }
+`;
