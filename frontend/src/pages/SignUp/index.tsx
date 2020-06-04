9de4c1ca0fc4eeb68486e2067cdb8bb6162788c5
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-04 07:49:11 -0300

    Implementa validação de erros

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,19 +1,25 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Container, Content, BackgroundImg } from './styles';
 
 const SingUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const handleSubmit = useCallback( async (data: object) => {
+
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome é obrigatório'),
         email: Yup.string().required('E-mail é obrigatório'),
@@ -27,6 +33,10 @@ const SingUp: React.FC = () => {
       console.log('data: ', data);
     } catch(err) {
       console.error(err)
+
+      const errors = getValidationErrors(err);
+      console.log('getValidationErrors: ', errors);
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -39,7 +49,7 @@ const SingUp: React.FC = () => {
           alt="Logo Awesome Go Barber Haircuts and shaves"
           title="Logo Go Barber"
         />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça o seu cadastro</h1>
           <Input
             name="name"
