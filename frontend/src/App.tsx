60bc58cc6a22c82d968f4e413e8e1828016a01d0
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-05 23:05:28 -0300

    Importa o contexto e provém para as rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 
+import AuthContext from './context/AuthContext'
 import GlobalStyle from './styles/global';
 import Routes from './routes/index'
 
 const App: React.FC = () => (
   <React.Fragment>
-    <BrowserRouter>
-      <Routes />
+    <AuthContext.Provider value={{
+      email: 'jean@gmail.com',
+      name: 'Jean'
+    }}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </AuthContext.Provider>
       <GlobalStyle />
-    </BrowserRouter>
   </React.Fragment>
 )
 
