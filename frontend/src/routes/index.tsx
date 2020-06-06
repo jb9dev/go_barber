89fe5b3bade5ab9efea8981be2659c9c2a76f959
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-05 23:55:48 -0300

    Contexto de autenticação somente nas devidas rotas

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import { AuthProvider } from '../context/AuthContext'
+
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
-      <Route path="/" exact component={SignIn} />
-      <Route path="/register" exact component={SignUp} />
+      <AuthProvider>
+        <Route path="/" exact component={SignIn} />
+        <Route path="/register" exact component={SignUp} />
+      </AuthProvider>
     </Switch>
   )
 };
