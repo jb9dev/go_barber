b08035647d4e5e6bb5c58c6cd462a0250299388b
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-05 23:55:25 -0300

    Considera possibilidade de múltiplos contextos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 
-import { AuthProvider } from './context/AuthContext'
 import GlobalStyle from './styles/global';
 import Routes from './routes/index'
 
 const App: React.FC = () => (
   <React.Fragment>
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </AuthProvider>
-      <GlobalStyle />
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
   </React.Fragment>
 )
 
