ea9d2b00f85a69a7cb0f2d0eb00cb78eb10eff80
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-08 08:24:18 -0300

    Importação do ToastContainer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter } from 'react-router-dom'
 
 import GlobalStyle from './styles/global';
 import Routes from './routes/index'
+import ToastContainer from './components/ToastContainer'
 
 const App: React.FC = () => (
   <React.Fragment>
     <BrowserRouter>
       <Routes />
+      <ToastContainer />
     </BrowserRouter>
     <GlobalStyle />
   </React.Fragment>
