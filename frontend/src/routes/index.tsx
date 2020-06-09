749858b48e6219d64b08a3fd9e89168edc4fa509
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-09 18:31:38 -0300

    Implementa rota personalizada, para verificação de acesso a rotas privadas

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import AppProvider from '../hooks';
 
+import Route from './Route';
+
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => {
+
   return (
     <Switch>
       <AppProvider>
-        <Route path="/" exact component={SignIn} />
-        <Route path="/register" exact component={SignUp} />
+        <Route path="/" exact component={SignIn}/>
+        <Route path="/register" exact component={SignUp}/>
+        <Route
+          path="/dashboard" component={Dashboard}  isPrivate={true} />
       </AppProvider>
     </Switch>
   )
