b9397e12e7105ef1f96e08161fe4ce71e2350c08
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-09 18:48:58 -0300

    Adiciona botão de logout para testes

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
+
+import { useAuth } from '../../hooks/auth';
 
 import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
-
+  const { signOut } = useAuth();
   return (
     <Container>
       <h1>Dashboard</h1>
+      <button onClick={signOut}><FiPower size={30} /></button>
     </Container>
   )
 };
--- frontend/src/pages/Dashboard/styles.ts
+++ frontend/src/pages/Dashboard/styles.ts
@@ -1,7 +1,23 @@
 import styled from 'styled-components';
 
-// import { colors } from '../../styles/variables';
+import { colors } from '../../styles/variables';
+
+const { light, red } = colors;
 
 export const Container = styled.div`
+  display: flex;
+  padding-top: 20px;
+  align-items: center;
+  justify-content: space-around;
 
+  button {
+    color: ${light};
+    background: transparent;
+    border: 0;
+    transition: color 200ms ease-out;
+
+    &:hover {
+      color: ${red};
+    }
+  }
 `;
