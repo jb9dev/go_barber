4fe4de7a367367627842d0fad87e0e591b4d9826
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-11 08:10:47 -0300

    Estiliza app como um todo

--- mobile/src/styles.ts
+++ mobile/src/styles.ts
@@ -1,13 +1,17 @@
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 
 import { colors } from './styles/variables';
 
 const { light, secondary } = colors;
 
-export const Container = styled.view`
+export const Container = styled.View`
+  flex: 1;
   background: ${secondary};
+  align-items: center;
+  justify-content: center;
 `;
 
-export const Title = styled.text`
+export const Title = styled.Text`
+  font-size: 30px;
   color: ${light};
 `;
