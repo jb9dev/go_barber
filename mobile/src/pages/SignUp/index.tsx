7e23794ee96fe005f3898b2c403156735a13afbe
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-12 09:25:48 -0300

    Implementa input e button

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container, Title } from './styles';
 
 const SignUp: React.FC = () => {
-
+  const handledSubmit = useCallback(() => {
+    console.log('Sign up submited')
+  }, [])
   return (
     <Container>
-      <Title>SignUp</Title>
+      <Title>Faça o seu cadastro</Title>
+      <Input name="name" placeholder="Nome" icon="user" />
+      <Input name="email" placeholder="Email" icon="mail" />
+      <Input name="password" placeholder="Senha" icon="lock" />
+      <Button onPress={handledSubmit}>Cadastrar</Button>
     </Container>
   );
 };
