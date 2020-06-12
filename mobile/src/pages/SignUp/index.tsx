f636d98a7cf3f08c609c25bbe0f5fec407397145
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-12 10:46:39 -0300

    Finaliza elementos e estilos do cadastro

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,22 +1,57 @@
 import React, { useCallback } from 'react';
+import {
+  Image,
+  ScrollView,
+  View,
+  KeyboardAvoidingView,
+  Platform
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import logoImg from '../../assets/logo.png';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Title } from './styles';
+import { colors } from '../../styles/variables';
+
+import { Container, Title, GoBack, GoBackText } from './styles';
 
 const SignUp: React.FC = () => {
   const handledSubmit = useCallback(() => {
     console.log('Sign up submited')
   }, [])
+
+  const handledGoBack = useCallback(() => {
+    console.log('handle go back')
+  }, [])
+
   return (
-    <Container>
-      <Title>Faça o seu cadastro</Title>
-      <Input name="name" placeholder="Nome" icon="user" />
-      <Input name="email" placeholder="Email" icon="mail" />
-      <Input name="password" placeholder="Senha" icon="lock" />
-      <Button onPress={handledSubmit}>Cadastrar</Button>
-    </Container>
+    <React.Fragment>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <Title>Crie sua conta</Title>
+            <Input name="name" placeholder="Nome" icon="user" />
+            <Input name="email" placeholder="Email" icon="mail" />
+            <Input name="password" placeholder="Senha" icon="lock" />
+            <Button onPress={handledSubmit}>Cadastrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <GoBack onPress={handledGoBack}>
+        <Icon name="arrow-left" size={20} color={colors.light2} />
+        <GoBackText>Voltar para logon</GoBackText>
+      </GoBack>
+    </React.Fragment>
   );
 };
 
--- mobile/src/pages/SignUp/styles.ts
+++ mobile/src/pages/SignUp/styles.ts
@@ -1,16 +1,43 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 import { colors, fontFamilies } from '../../styles/variables';
 
-const { light2 } = colors;
+const { dark, light2, secondary } = colors;
 
 export const Container = styled.View`
-  padding-top: 20px;
+  flex: 1;
+  margin-bottom: ${Platform.OS === 'android' ? 150 : 40}px;
+  padding: 20px;
   align-items: center;
+  justify-content: center;
 `;
 
 export const Title = styled.Text`
+  margin: 64px 0 16px;
   font-family: ${fontFamilies.medium};
   font-size: 25px;
+  color: ${light2};
+`;
+
+export const GoBack = styled.TouchableOpacity`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  flex-direction: row;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${secondary};
+  border-top-width: 1px;
+  border-top-color: ${dark};
+`;
+
+export const GoBackText = styled.Text`
+  margin-left: 16px;
+  font-family: ${fontFamilies.medium};
+  font-size: 16px;
   color: ${light2};
 `;
