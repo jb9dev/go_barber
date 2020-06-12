ee658140ae07d0ae47eb12b5af9b096a6f5712e4
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-12 09:17:28 -0300

    Estilização para receber inputs e button

--- mobile/src/pages/SignIn/styles.ts
+++ mobile/src/pages/SignIn/styles.ts
@@ -1,23 +1,19 @@
 import styled from 'styled-components/native';
-import Icon from 'react-native-vector-icons/Feather';
 
-import { colors } from '../../styles/variables';
+import { colors, fontFamilies } from '../../styles/variables';
 
 const { light2 } = colors;
 
 export const Container = styled.View`
   flex: 1;
+  padding: 0 20px;
   align-items: center;
   justify-content: center;
 `;
 
 export const Title = styled.Text`
   margin: 64px 0 16px;
-  font-family: 'RobotoSlab-Medium';
+  font-family: ${fontFamilies.medium};
   font-size: 25px;
-  color: ${light2};
-`;
-
-export const InputIcon = styled(Icon)`
   color: ${light2};
 `;
