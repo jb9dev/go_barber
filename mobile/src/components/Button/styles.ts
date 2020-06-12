99a751304ba6a04ee33a82f2837e4a930ccf842d
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-12 09:17:07 -0300

    Criação do componente button para ser reutilizado

---
+++ mobile/src/components/Button/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import { colors, fontFamilies } from '../../styles/variables';
+
+const { primary, secondary } = colors;
+
+export const Container = styled(RectButton)`
+  width: 100%;
+  height: 60px;
+  margin-top: 8px;
+  padding: 0 16px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${primary};
+  border-radius: 6px;
+`;
+
+export const ButtonText = styled.Text`
+  font-family: ${fontFamilies.medium};
+  font-size: 18px;
+  color: ${secondary};
+`;
