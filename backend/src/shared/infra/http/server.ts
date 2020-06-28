785e955f1bd6844f865360144e8398aaba77eb8c
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-28 00:27:38 -0300

    Importa container de injeção de dependências

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,12 +1,13 @@
+import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
 import AppError from '@shared/errors/AppError';
 import routes from './routes';
-import 'reflect-metadata';
 
 import '@shared/infra/typeorm';
+import '@shared/container';
 
 const server = express();
 
