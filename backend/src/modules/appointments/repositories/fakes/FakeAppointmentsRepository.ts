c05b03b652aba3af43440772c77227a2ecb4f344
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-29 16:53:01 -0300

    Corrige inclusão e consulta dos appointments

--- backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,15 +1,16 @@
 import { uuid } from 'uuidv4';
+import { isEqual } from 'date-fns';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
-class AppointmentsRepository implements IAppointmentsRepository {
+class FakeAppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const appointmentByDate = this.appointments.find(
-      appointment => appointment.date === date,
+    const appointmentByDate = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
     );
 
     return appointmentByDate;
@@ -20,11 +21,10 @@ class AppointmentsRepository implements IAppointmentsRepository {
     date,
   }: ICreateAppointmentDTO): Promise<Appointment> {
     const appointment = new Appointment();
-
     Object.assign(appointment, { id: uuid(), provider_id, date });
-
+    this.appointments.push(appointment);
     return appointment;
   }
 }
 
-export default AppointmentsRepository;
+export default FakeAppointmentsRepository;
