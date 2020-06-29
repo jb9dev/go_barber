eae958e5a05877e3b337cceda6be3ebbb4e055dd
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-06-29 14:45:09 -0300

    Cria repositório fake para testes

---
+++ backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,30 @@
+import { uuid } from 'uuidv4';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const appointmentByDate = this.appointments.find(
+      appointment => appointment.date === date,
+    );
+
+    return appointmentByDate;
+  }
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: uuid(), provider_id, date });
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
