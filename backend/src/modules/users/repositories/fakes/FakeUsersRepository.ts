94b22c165315a390b8a59731f93f8427ec5947c5
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-07-21 10:49:32 -0300

    Cria repositório de usuários para testes

---
+++ backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,36 @@
+import { uuid } from 'uuidv4';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+class UsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = this.users.find(findUser => findUser.email === email);
+    return user;
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    const user = this.users.find(findUser => findUser.id === id);
+    return user;
+  }
+
+  public async create(userData: ICreateUserDTO): Promise<User> {
+    const user = new User();
+    Object.assign(user, { id: uuid() }, userData);
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+    this.users[findIndex] = user;
+    return user;
+  }
+}
+
+export default UsersRepository;
