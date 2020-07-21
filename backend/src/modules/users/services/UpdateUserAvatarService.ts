3beebad704b000d5d2ce80d4e417fd54775634a4
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-07-21 11:59:19 -0300

    Implementa provedor de armazenamento

--- backend/src/modules/users/services/UpdateUserAvatarService.ts
+++ backend/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,9 +1,7 @@
-import path from 'path';
-import fs from 'fs';
-import uploadConfig from '@config/upload';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
 
@@ -17,6 +15,9 @@ class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute({
@@ -30,15 +31,11 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(avatar_filename);
     }
 
-    user.avatar = avatar_filename;
+    const fileName = await this.storageProvider.saveFile(avatar_filename);
+    user.avatar = fileName;
     await this.usersRepository.save(user);
 
     return user;
