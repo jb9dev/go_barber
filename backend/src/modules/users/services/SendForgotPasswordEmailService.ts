1b07cb274f718094d9a8e68259dc63a3a99cddc1
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-07-24 09:48:54 -0300

    Verifica existência do usuário e gera token

--- backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,9 +1,10 @@
 import { injectable, inject } from 'tsyringe';
 
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 // import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
 interface IRequestDTO {
   email: string;
@@ -17,9 +18,20 @@ class SendForgotPasswordEmailService {
 
     @inject('MailProvider')
     private mailhProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({ email }: IRequestDTO): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists');
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
     await this.mailhProvider.sendEmail(
       email,
       'Recebemos a sua solicitação de recuperação de senha com sucesso!',
