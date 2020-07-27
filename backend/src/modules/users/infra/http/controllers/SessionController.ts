0d68b118f730dd315bb72b72dfdd87518149ec55
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-07-27 08:17:05 -0300

    Cria controller para sessão de usuário

---
+++ backend/src/modules/users/infra/http/controllers/SessionController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import AuthenicateUserService from '@modules/users/services/AuthenicateUserService';
+
+export default class SessionController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+    const authenicateUserService = container.resolve(AuthenicateUserService);
+
+    const { user, token } = await authenicateUserService.execute({
+      email,
+      password,
+    });
+
+    return response.json({ user, token });
+  }
+}
