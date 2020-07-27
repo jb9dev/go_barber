3934e757c3886f3c654a584beb0bc03ed4a89d90
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-07-26 21:36:19 -0300

    Implementa novo método, conforme alteração na interface

--- backend/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ backend/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -9,13 +9,21 @@ class FakeUserTokensRepository implements IUserTokensRepository {
   public async generate(user_id: string): Promise<UserToken> {
     const userToken = new UserToken();
 
-    Object.assign(UserToken, {
+    Object.assign(userToken, {
       id: uuid(),
       token: uuid(),
       user_id,
     });
 
     this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      foundToken => foundToken.token === token,
+    );
 
     return userToken;
   }
