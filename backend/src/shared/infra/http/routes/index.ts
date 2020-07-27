8f1eb3fc06896d79e3a9e2b8eebe5b7a2d2d8fec
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-07-27 08:18:54 -0300

    Importa rotas de senha

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -4,12 +4,14 @@ import uploadConfig from '@config/upload';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/session.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
-routes.use('/files', express.static(uploadConfig.directory));
+routes.use('/files', express.static(uploadConfig.tempDir));
+routes.use('/password', passwordRouter);
 
 export default routes;
