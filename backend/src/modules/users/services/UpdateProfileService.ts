6896ffba616cb72f3ec8400ca2392649b76d8e13
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-07-28 00:43:49 -0300

    Cria serviço de atualização de perfil do usuário

---
+++ backend/src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,68 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import User from '@modules/users/infra/typeorm/entities/User';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequestDTO {
+  user_id: string;
+  name: string;
+  email: string;
+  old_password?: string;
+  password?: string;
+}
+
+@injectable()
+class UpdateProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({
+    user_id,
+    name,
+    email,
+    old_password,
+    password,
+  }: IRequestDTO): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+    const updatedUser = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists');
+    }
+
+    if (updatedUser && updatedUser.id !== user_id) {
+      throw new AppError('This e-mail is already in use');
+    }
+
+    if (password && !old_password) {
+      throw new AppError('You must inform the old password');
+    }
+
+    if (password && old_password) {
+      const matchOldPassword = await this.hashProvider.compareHash(
+        old_password,
+        user.password,
+      );
+
+      if (!matchOldPassword) {
+        throw new AppError('Old password does not match');
+      }
+
+      user.password = await this.hashProvider.generateHash(password);
+    }
+
+    user.name = name;
+    user.email = email;
+
+    return this.usersRepository.save(user);
+  }
+}
+
+export default UpdateProfileService;
