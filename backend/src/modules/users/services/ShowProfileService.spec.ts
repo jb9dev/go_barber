f504ffbdab9307248f193b518965ba504ba09d72
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-07-28 07:55:02 -0300

    Testa exibição do perfil do usuário

---
+++ backend/src/modules/users/services/ShowProfileService.spec.ts
@@ -0,0 +1,39 @@
+import 'reflect-metadata';
+
+import AppError from '@shared/errors/AppError';
+
+import ShowProfileService from './ShowProfileService';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+
+let fakeUsersRepository: FakeUsersRepository;
+let showProfile: ShowProfileService;
+
+describe('ShowProfile', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    showProfile = new ShowProfileService(fakeUsersRepository);
+  });
+
+  it('should be able to show the profile', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@test.com',
+      password: '123456',
+    });
+
+    await showProfile.execute({
+      user_id: user.id,
+    });
+
+    expect(user.name).toBe('John Doe');
+    expect(user.email).toBe('johndoe@test.com');
+  });
+
+  it('should not be able to show the profile from a non-existing user', async () => {
+    await expect(
+      showProfile.execute({
+        user_id: 'non-existing-user-id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
