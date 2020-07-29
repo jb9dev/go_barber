cef7788c67b34c7441abd970d37d966717643dd9
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-07-29 08:55:49 -0300

    Novo método para encontrar disponibilidade no mês para o prestador de serviço

--- backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,8 +1,9 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository, Raw } from 'typeorm';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 
 class AppointmentsRepository implements IAppointmentsRepository {
   private ormRepository: Repository<Appointment>;
@@ -17,6 +18,26 @@ class AppointmentsRepository implements IAppointmentsRepository {
     });
 
     return appointmentByDate;
+  }
+
+  public async findAllInMonthFromProvider({
+    provider_id,
+    month,
+    year,
+  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+    const parsedMonth = String(month).padStart(2, '0');
+
+    const appointmentsByProviders = this.ormRepository.find({
+      where: {
+        provider_id,
+        date: Raw(
+          dateField =>
+            `to_char(${dateField}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
+        ),
+      },
+    });
+
+    return appointmentsByProviders;
   }
 
   public async create({
