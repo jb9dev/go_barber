18550375ee198c2cb41c1b6daa346048f6bfe045
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-07-30 22:49:59 -0300

    Rota para listagem de agendamentos de um prestador

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import ensureAuthenicated from '@modules/users/infra/http/middlewares/ensureAuthenicated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenicated);
 
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
