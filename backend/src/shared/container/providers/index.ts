02042cee9e74f3de3ab02eea0e58792dead09cf6
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-07-31 22:29:00 -0300

    Separando injeção de dependência por provider Para melhor organização

--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -1,28 +1,3 @@
-import { container } from 'tsyringe';
-
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-
-import IMailProvider from './MailProvider/models/IMailProvider';
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
-import SESMailProvider from './MailProvider/implementations/SESMailProvider';
-
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarsTemplateProvider from './MailTemplateProvider/implementations/HandlebarsTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsTemplateProvider,
-);
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  process.env.APP_MAIL_DRIVER === 'ethereal'
-    ? container.resolve(EtherealMailProvider)
-    : container.resolve(SESMailProvider),
-);
+import './StorageProvider';
+import './MailTemplateProvider';
+import './MailProvider';
