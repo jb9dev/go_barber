0a2fc94ee97bbee9855b3bb38f16cb89398b0ae8
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-08-01 23:03:12 -0300

    Adiciona configurações para upload no Amazon S3

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,19 +1,43 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
 const tempDir = path.resolve(__dirname, '..', '..', 'tmp');
 const uploadsDir = path.resolve(tempDir, 'uploads');
 
+interface IUploadConfig {
+  driver: 'disk' | 's3';
+  tempDir: string;
+  uploadsDir: string;
+  multer: {
+    storage: StorageEngine;
+  };
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
+
 export default {
+  driver: process.env.APP_STORAGE_DRIVER,
   tempDir,
   uploadsDir,
-  storage: multer.diskStorage({
-    destination: tempDir,
-    filename(request, file, callback) {
-      const hash = crypto.randomBytes(10).toString('HEX');
-      const filename = `${hash}-${file.originalname}`;
-      return callback(null, filename);
+  multer: {
+    storage: multer.diskStorage({
+      destination: tempDir,
+      filename(request, file, callback) {
+        const hash = crypto.randomBytes(10).toString('HEX');
+        const filename = `${hash}-${file.originalname}`;
+        return callback(null, filename);
+      },
+    }),
+  },
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber', // precisa criar na aws com esse nome
     },
-  }),
-};
+  },
+} as IUploadConfig;
