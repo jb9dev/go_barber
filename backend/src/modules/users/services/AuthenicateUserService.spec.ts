9e0f0920e4bc314c89c3a5062f0a8cc735f20435
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-08-03 12:30:52 -0300

    Cria usuário direto a partir de repositório fake

--- backend/src/modules/users/services/AuthenicateUserService.spec.ts
+++ backend/src/modules/users/services/AuthenicateUserService.spec.ts
@@ -1,29 +1,19 @@
 import 'reflect-metadata';
 
 import AppError from '@shared/errors/AppError';
-import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider';
 
-import CreateUserService from './CreateUserService';
 import AuthenicateUserService from './AuthenicateUserService';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 
 let fakeUsersRepository: FakeUsersRepository;
 let fakeHashProvider: FakeHashProvider;
-let fakeCacheProvider: FakeCacheProvider;
-let createUserService: CreateUserService;
 let authenicateUserService: AuthenicateUserService;
 
 describe('CreateAppointment', () => {
   beforeEach(() => {
     fakeUsersRepository = new FakeUsersRepository();
     fakeHashProvider = new FakeHashProvider();
-    fakeCacheProvider = new FakeCacheProvider();
-    createUserService = new CreateUserService(
-      fakeUsersRepository,
-      fakeHashProvider,
-      fakeCacheProvider,
-    );
     authenicateUserService = new AuthenicateUserService(
       fakeUsersRepository,
       fakeHashProvider,
@@ -31,7 +21,7 @@ describe('CreateAppointment', () => {
   });
 
   it('should be able to authenicate a user', async () => {
-    const user = await createUserService.execute({
+    const user = await fakeUsersRepository.create({
       name: 'John Doe',
       email: 'johndoe@test.com',
       password: '123456',
@@ -56,7 +46,7 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to authenicate a user with wrong password', async () => {
-    await createUserService.execute({
+    await fakeUsersRepository.create({
       name: 'John Doe',
       email: 'johndoe@test.com',
       password: '123456',
