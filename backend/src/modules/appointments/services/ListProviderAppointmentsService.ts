30d4606b44e212626af353792cbb673ba30aa0a9
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-08-04 08:17:29 -0300

    Serializa dados de usuário Na resposta da API e para o cache

--- backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
@@ -43,10 +44,10 @@ class ListProviderAppointmentsService {
         },
       );
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
-    return appointments;
+    return classToClass(appointments);
   }
 }
 
