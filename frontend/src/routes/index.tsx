9873ff52645da47d755c1b6d78b1f843de9f1fa8
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-08-05 11:00:57 -0300

    Cria rota para página de resetar senha

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -8,6 +8,7 @@ import Route from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => {
@@ -18,6 +19,7 @@ const Routes: React.FC = () => {
         <Route path="/" exact component={SignIn}/>
         <Route path="/register" exact component={SignUp}/>
         <Route path="/forgot-password" exact component={ForgotPassword}/>
+        <Route path="/reset-password" exact component={ResetPassword}/>
         <Route
           path="/dashboard" component={Dashboard}  isPrivate={true} />
       </AppProvider>
