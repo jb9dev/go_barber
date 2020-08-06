764c1c38c0c50806c985d26b24c9a0c5756ad784
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-08-06 00:51:49 -0300

    Melhora a estilização das setas para trocar o mês

--- frontend/src/pages/Dashboard/styles.ts
+++ frontend/src/pages/Dashboard/styles.ts
@@ -236,9 +236,15 @@ export const Calendar = styled.aside`
       .DayPicker-NavButton {
         top: 17px;
         margin-top: 0;
+        background-size: 20px;
 
         &--prev {
           left: 20px;
+          background-image: url('data:image/svg+xml,%3Csvg%20stroke%3D%22%23ffffff%22%20fill%3D%22none%22%20stroke-width%3D%222%22%20viewBox%3D%220%200%2024%2024%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%20height%3D%221em%22%20width%3D%221em%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cline%20x1%3D%2219%22%20y1%3D%2212%22%20x2%3D%225%22%20y2%3D%2212%22%3E%3C%2Fline%3E%3Cpolyline%20points%3D%2212%2019%205%2012%2012%205%22%3E%3C%2Fpolyline%3E%3C%2Fsvg%3E');
+        }
+
+        &--next {
+          background-image: url('data:image/svg+xml,%3Csvg%20stroke%3D%22%23ffffff%22%20fill%3D%22none%22%20stroke-width%3D%222%22%20viewBox%3D%220%200%2024%2024%22%20stroke-linecap%3D%22round%22%20%20stroke-linejoin%3D%22round%22%20height%3D%221em%22%20width%3D%221em%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cline%20x1%3D%225%22%20y1%3D%2212%22%20x2%3D%2219%22%20y2%3D%2212%22%3E%3C%2Fline%3E%3Cpolyline%20points%3D%2212%205%2019%2012%2012%2019%22%3E%3C%2Fpolyline%3E%3C%2Fsvg%3E%0A');
         }
       }
     }
