dbba4dab73df5bf559afbb44bd934dabd49bd226
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-08-07 14:55:25 -0300

    Adiciona novas cores

--- mobile/src/globalVariables.ts
+++ mobile/src/globalVariables.ts
@@ -1,12 +1,15 @@
-import { rgba } from "polished";
+import { rgba } from 'polished';
 
 export const colors = {
   primary: '#ff9000',
   secondary: '#312e38',
   blue: '#3172b7',
   dark: '#232129',
+  dark2: '#28262e',
   green: '#2e656a',
+  lightGrey: '#999591',
   grey: '#666360',
+  darkGrey: '#3e3b47',
   light: '#ffffff',
   light2: '#f4ede8',
   lightestBlue: '#ebf8ff',
@@ -19,4 +22,4 @@ export const colors = {
 export const fontFamilies = {
   regular: 'RobotoSlab-Regular',
   medium: 'RobotoSlab-Medium',
-}
+};
