205d1d81faedb36eae57e7c7c248c5e131d36831
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-08-07 13:02:38 -0300

    Configura rotas para as telas da aplicação

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Dashboard from '../pages/Dashboard';
+import CreateAppointment from '../pages/CreateAppointment';
+import AppointmentCreated from '../pages/AppointmentCreated';
+import Profile from '../pages/Profile';
 
 import { colors } from '../globalVariables';
 
@@ -12,10 +15,14 @@ const AppRoutes: React.FC = () => (
   <App.Navigator
     screenOptions={{
       headerShown: false,
-      cardStyle: { backgroundColor: secondary }
+      cardStyle: { backgroundColor: secondary },
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
