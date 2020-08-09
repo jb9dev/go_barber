6ab514f62a4510c08a6095954fd2cf8e0b10f37c
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-08-08 21:16:29 -0300

    Cria listagem de providers

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 
+import api from '../../services/api';
 import { useAuth } from '../../hooks/auth';
 
 import {
@@ -10,11 +11,25 @@ import {
   UserName,
   ProfileButton,
   UserAvatar,
+  ProvidersList,
 } from './styles';
 
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 const Dashboard: React.FC = () => {
+  const [providers, setProviders] = useState<Provider[]>([]);
   const { signOut, user } = useAuth();
   const { navigate } = useNavigation();
+
+  useEffect(() => {
+    api.get('/providers').then((response) => {
+      setProviders(response.data);
+    });
+  }, []);
 
   const navigateToProfile = useCallback(() => {
     navigate('Profile');
@@ -32,6 +47,11 @@ const Dashboard: React.FC = () => {
           <UserAvatar source={{ uri: user.avatar_url }} />
         </ProfileButton>
       </Header>
+      <ProvidersList
+        keyExtractor={(provider) => provider.id}
+        data={providers}
+        renderItem={({ item }) => <UserName>{item.name}</UserName>}
+      />
     </Container>
   );
 };
