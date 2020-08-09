23cf48ab4f092877c6860a3df072ac5347b4e4d3
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-08-08 21:15:39 -0300

    Salva automaticamente bearer token ao logar

--- mobile/src/hooks/auth.tsx
+++ mobile/src/hooks/auth.tsx
@@ -53,6 +53,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -70,6 +72,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:Token', token],
       ['@GoBarber:User', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
