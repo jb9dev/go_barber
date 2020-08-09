ed681a82d3d7a4a87d8937f422e3f36bcdafc60d
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-08-09 09:24:04 -0300

    Inicia criação da página de criação do agendamento

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -1,9 +1,140 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-import { Container } from './styles';
+import { useAuth } from '../../hooks/auth';
+import Provider from '../../interfaces/Provider';
+import api from '../../services/api';
+
+import { colors } from '../../globalVariables';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+  UserAvatarContainer,
+  ProvidersListContainer,
+  ProvidersList,
+  ProviderContainer,
+  ProviderAvatarContainer,
+  ProviderAvatar,
+  ProviderName,
+  Calendar,
+  Title,
+} from './styles';
+
+interface RouteParams {
+  providerId: string;
+}
+
+interface MonthAvailability {
+  available: boolean;
+  day: number;
+}
+interface DayAvailability {
+  available: boolean;
+  hour: number;
+}
 
 const CreateAppointment: React.FC = () => {
-  return <Container />;
+  const { user } = useAuth();
+  const { goBack } = useNavigation();
+  const route = useRoute();
+  const routeParams = route.params as RouteParams;
+  const [providers, setProviders] = useState<Provider[]>([]);
+  const [selectedProvider, setSelectedProvider] = useState(
+    routeParams.providerId,
+  );
+  const today = new Date();
+  const [providerDayAvailability, setProviderDayAvailability] = useState<
+    DayAvailability[]
+  >([]);
+  const [providerMonthAvailability, setProviderMonthAvailability] = useState<
+    DayAvailability[]
+  >([]);
+
+  const handleBackNavigation = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  useEffect(() => {
+    api.get('/providers').then((response) => {
+      setProviders(response.data);
+    });
+  }, []);
+
+  // useEffect(() => {
+  //   api
+  //     .get(`/availability/${selectedProvider}/month`, {
+  //       params: {
+  //         year: today.getFullYear(),
+  //         month: today.getMonth() + 1,
+  //       },
+  //     })
+  //     .then((response) => {
+  //       setProviderMonthAvailability(response.data);
+  //     });
+  // }, [selectedProvider, today]);
+
+  const handleSelectProvider = useCallback((providerId: string) => {
+    setSelectedProvider(providerId);
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={handleBackNavigation}>
+          <Icon name="chevron-left" size={24} color={colors.lightGrey} />
+        </BackButton>
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+        <UserAvatarContainer>
+          {user.avatar_url ? (
+            <UserAvatar source={{ uri: user.avatar_url }} />
+          ) : (
+            <Icon name="camera" size={24} color={colors.darkGrey} />
+          )}
+        </UserAvatarContainer>
+      </Header>
+      <ProvidersListContainer>
+        <ProvidersList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(provider) => provider.id}
+          data={providers}
+          renderItem={({ item: provider }) => (
+            <ProviderContainer
+              onPress={() => handleSelectProvider(provider.id)}
+              selected={selectedProvider === provider.id}
+            >
+              <ProviderAvatarContainer>
+                {provider.avatar_url ? (
+                  <ProviderAvatar source={{ uri: provider.avatar_url }} />
+                ) : (
+                  <Icon name="camera" size={16} color={colors.darkGrey} />
+                )}
+              </ProviderAvatarContainer>
+              <ProviderName selected={selectedProvider === provider.id}>
+                {provider.name}
+              </ProviderName>
+            </ProviderContainer>
+          )}
+        />
+      </ProvidersListContainer>
+      <Calendar>
+        <Title>Escolha a data</Title>
+        <DateTimePicker
+          onChange={() => {}}
+          display="calendar"
+          textColor={colors.light2}
+          mode="date"
+          value={today}
+        />
+      </Calendar>
+    </Container>
+  );
 };
 
 export default CreateAppointment;
--- mobile/src/pages/CreateAppointment/styles.ts
+++ mobile/src/pages/CreateAppointment/styles.ts
@@ -1,3 +1,97 @@
+import { FlatList } from 'react-native';
 import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import { RectButton } from 'react-native-gesture-handler';
+import Provider from '../../interfaces/Provider';
+
+import { colors, fontFamilies } from '../../globalVariables';
+
+interface ProviderContainerProps {
+  selected: boolean;
+}
+interface ProviderNameProps {
+  selected: boolean;
+}
 
 export const Container = styled.View``;
+
+export const Header = styled.View`
+  flex-direction: row;
+  padding: 20px;
+  padding-top: ${getStatusBarHeight() + 24}px;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${colors.dark2};
+`;
+
+export const BackButton = styled.TouchableOpacity``;
+
+export const HeaderTitle = styled.Text`
+  margin-left: 16px;
+  font-family: ${fontFamilies.medium};
+  font-size: 20px;
+  color: ${colors.light2};
+`;
+
+export const UserAvatarContainer = styled.View`
+  width: 58px;
+  height: 58px;
+  margin-left: auto;
+  align-items: center;
+  justify-content: center;
+  background-color: ${colors.lightGrey};
+  border-radius: 29px;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 58px;
+  height: 58px;
+  border-radius: 29px;
+`;
+
+export const ProvidersListContainer = styled.View`
+  flex-direction: row;
+  height: 112px;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px;
+`;
+
+export const ProviderContainer = styled(RectButton)<ProviderContainerProps>`
+  flex-direction: row;
+  margin-right: 32px;
+  padding: 10px;
+  align-items: center;
+  background-color: ${(props) =>
+    props.selected ? colors.primary : colors.dark2};
+  border-radius: 10px;
+`;
+export const ProviderAvatarContainer = styled.View`
+  width: 32px;
+  height: 32px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${colors.lightGrey};
+  border-radius: 16px;
+`;
+export const ProviderAvatar = styled.Image`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+`;
+export const ProviderName = styled.Text<ProviderNameProps>`
+  margin-left: 8px;
+  font-family: ${fontFamilies.medium};
+  font-size: 16px;
+  color: ${(props) => (props.selected ? colors.dark : colors.light2)};
+`;
+
+export const Calendar = styled.View``;
+
+export const Title = styled.Text`
+  font-family: ${fontFamilies.medium};
+  font-size: 32px;
+  color: ${colors.light2};
+`;
