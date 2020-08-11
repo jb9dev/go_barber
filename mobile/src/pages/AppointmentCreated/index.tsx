02298940ae0cbcd47fc77f22ef35785698149f81
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-08-11 09:04:33 -0300

    Finaliza tela de conclusão de criação de agenda/o

--- mobile/src/pages/AppointmentCreated/index.tsx
+++ mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-import { Container } from './styles';
+import { colors } from '../../globalVariables';
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
+
+interface RouteParams {
+  date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
-  return <Container />;
+  const { reset } = useNavigation();
+  const { params } = useRoute();
+  const routeParams = params as RouteParams;
+
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE ', dia' dd 'de' MMMM 'de' yyyy 'às' HH'h'",
+      { locale: ptBR },
+    );
+  }, [routeParams.date]);
+
+  const handleOk = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color={colors.lightGreen} />
+      <Title>Agendamento concluído</Title>
+      <Description>{formattedDate}</Description>
+      <OkButton onPress={handleOk}>
+        <OkButtonText>ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
--- mobile/src/pages/AppointmentCreated/styles.ts
+++ mobile/src/pages/AppointmentCreated/styles.ts
@@ -1,3 +1,43 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View``;
+import { colors, fontFamilies } from '../../globalVariables';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 0 24px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  margin-top: 48px;
+  font-family: ${fontFamilies.robotoMedium};
+  font-size: 32px;
+  color: ${colors.light2};
+  text-align: center;
+`;
+
+export const Description = styled.Text`
+  margin-top: 16px;
+  font-family: ${fontFamilies.robotoMedium};
+  font-size: 18px;
+  color: ${colors.lightGrey};
+  text-align: center;
+`;
+
+export const OkButton = styled(RectButton)`
+  margin-top: 24px;
+  padding: 12px 24px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  background-color: ${colors.primary};
+`;
+
+export const OkButtonText = styled.Text`
+  font-family: ${fontFamilies.robotoMedium};
+  font-size: 18px;
+  color: ${colors.secondary};
+  text-transform: uppercase;
+`;
