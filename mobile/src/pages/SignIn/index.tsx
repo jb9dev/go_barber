ad2019d4104c4837389ff139b815391b7801dfc6
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-08-12 23:06:41 -0300

    Melhora iteração de validação do formulário, e
    Anota funcionalidades que ficaram faltando, como tela de reset de senha

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -39,60 +39,67 @@ interface SignInFormData {
 }
 
 const SignIn: React.FC = () => {
-  const navigation = useNavigation();
+  const { navigate } = useNavigation();
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
   const { signIn } = useAuth();
 
-  const handleSignIn = useCallback(async (data: SignInFormData) => {
-    try {
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('E-mail é obrigatório')
-          .email('Digite um e-mail válido'),
-        password: Yup.string().min(6, 'Mínimo de 6 caracteres'),
-      });
+  const handleSignIn = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail é obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().min(
+            6,
+            'Digite uma senha com no mínimo de 6 caracteres',
+          ),
+        });
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
+        await schema.validate(data, {
+          abortEarly: false,
+        });
 
-      await signIn(data);
-      formRef.current?.reset();
-    } catch (err) {
-      if (err instanceof Yup.ValidationError) {
-        const errors = getValidationErrors(err);
-        const errorMessages = [];
-        for (const error in errors) {
-          errorMessages.push(errors[error]);
+        await signIn(data);
+        formRef.current?.reset();
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const validationErrors = getValidationErrors(err);
+          const errorMessages = Object.values(validationErrors);
+
+          formRef.current?.setErrors(validationErrors);
+          Alert.alert(
+            'Login inválido!',
+            `Ocorreu um erro ao realizar o login, verifique as credenciais pois: ${errorMessages.join(
+              '; ',
+            )}.`,
+            [{ text: 'ok', style: 'default' }],
+          );
+          return;
         }
 
-        formRef.current?.setErrors(errors);
         Alert.alert(
           'Login inválido!',
-          `Ocorreu um erro ao realizar o login, verifique as credenciais pois: ${errorMessages.join(
-            '; ',
-          )}.`,
+          'Ocorreu um erro ao realizar o login, verifique suas credenciais.',
           [{ text: 'ok', style: 'default' }],
         );
-        return;
       }
-
-      Alert.alert(
-        'Login inválido!',
-        'Ocorreu um erro ao realizar o login, verifique suas credenciais.',
-        [{ text: 'ok', style: 'default' }],
-      );
-    }
-  }, []);
+    },
+    [signIn],
+  );
 
   const handleForgotPassword = useCallback(() => {
-    console.log('Handle forgot password');
+    // TODO
+    Alert.alert(
+      'Falta criar tela!',
+      'Criar tela de resetar a senha. Provavelmente precisará de uma nova feature no back-end, para suportar o reset pelo aplicativo, pois não teria como redirecionar do e-mail. Ou então ver uma forma de identificar se o usuário possui o aplicativo instalado, a partir do link que chega para o e-mail, mas de alguma forma precisaria passar o token como parâmetro da rota.',
+    );
   }, []);
 
   const handleCreateAccount = useCallback(() => {
-    navigation.navigate('SignUp');
-  }, []);
+    navigate('SignUp');
+  }, [navigate]);
 
   return (
     <>
