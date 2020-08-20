8394bb4ec0642ff4ac00167d3f1b895e1cad0350
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-08-20 00:53:16 -0300

    Corrige verificação de e-mail válido

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -30,7 +30,7 @@ const SingIn: React.FC = () => {
   const handleSubmit = useCallback( async (data: SignInFormData) => {
     try {
       const schema = Yup.object().shape({
-        email: Yup.string().required('E-mail é obrigatório'),
+        email: Yup.string().required('E-mail é obrigatório').email('Digite um e-mail válido'),
         password: Yup.string().min(6, 'Mínimo de 6 caracteres')
       });
 
