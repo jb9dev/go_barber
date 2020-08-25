48e4cf0d2c6bb8b4d2eb1b1fc88577e847c59b93
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-08-24 22:36:56 -0300

    Atributo para seletor do teste

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -48,6 +48,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon,...props }) => {
       hasError={!!error}
       isFocused={isFocused}
       isFilled={isFilled}
+      data-testid='input-container'
     >
       { Icon && <Icon /> }
       <input
